32d96073089881550743b780f376c7b74ed50ea1
atom <410340356@qq.com>
2023-01-10 11:11:24 +0800

    2023.1.10 学习新锋商城v1开发-文件上传测试1

--- src/main/java/ltd/newbee/mall/config/NeeBeeMallWebMvcConfigurer.java
+++ src/main/java/ltd/newbee/mall/config/NeeBeeMallWebMvcConfigurer.java
@@ -4,6 +4,7 @@ import ltd.newbee.mall.interceptor.AdminLoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -20,5 +21,14 @@ public class NeeBeeMallWebMvcConfigurer implements WebMvcConfigurer {
                 .excludePathPatterns("/admin/login")
                 .excludePathPatterns("/admin/dist/**")
                 .excludePathPatterns("/admin/plugins/**");
+    }
+
+    /**
+     * 增加一个自定义静态资源映射配置，让上传的文件可以被前端访问到
+     * 所有以“/upload/”开头的静态资源在请求时都会映射到D盘的upload目录下
+     * @param registry
+     */
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/upload/**").addResourceLocations("file:D:\\upload\\");
     }
 }
---
+++ src/main/java/ltd/newbee/mall/controller/UploadFileTestController.java
@@ -0,0 +1,119 @@
+package ltd.newbee.mall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+@Controller
+public class UploadFileTestController {
+
+    private static final String FILE_UPLOAD_PATH = "D:\\upload\\";
+
+    /**
+     * 上传多个文件，且前端标签名相同
+     * 1.判断上传的文件是否为空
+     * 2.判断是否超过文件数量上传限制
+     * 3.生成随机文件名（同uploadFile方法）
+     * 4.保存文件（同uploadFile方法）
+     * @param files
+     * @return
+     */
+    @PostMapping("/uploadFilesBySameName")
+    @ResponseBody
+    public String uploadFilesBySameName(@RequestPart("files") MultipartFile[] files){
+        //1.判断上传的文件是否为空
+        if(files == null || files.length == 0){
+            return "文件上传异常";
+        }
+        //2.判断是否超过文件数量上传限制
+        if(files.length > 5){
+            return "最多上传5个文件";
+        }
+        //3.生成随机文件名（同uploadFile方法）
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        Random r = new Random();
+        String newFileName = "";
+        //响应内容
+        String resultMsg = "文件上传成功, 路径如下: <br>";
+
+        for(MultipartFile file: files){
+            //判断文件名是否为空
+            String fileName = file.getOriginalFilename();
+            if(!StringUtils.hasText(fileName)){
+                //表示无文件信息，跳出当前循环
+                continue;
+            }
+            //获取文件后缀名
+            String suffixName = fileName.substring(fileName.lastIndexOf("."));
+            StringBuilder tempName = new StringBuilder();
+            tempName.append(sdf.format(new Date())).append(r.nextInt(100)).append(suffixName);
+            //生成新文件名
+            newFileName = tempName.toString();
+
+            try {
+                //保存文件
+                byte[] bytes = file.getBytes();
+                Path path = Paths.get(FILE_UPLOAD_PATH, newFileName);
+                Files.write(path, bytes);
+                resultMsg += fileName + " ---> /upload/" + newFileName + "<br>";
+            } catch (IOException e) {
+                e.printStackTrace();
+                resultMsg += fileName + "文件保存失败 <br>";
+            }
+        }
+        return resultMsg;
+    }
+
+    /**
+     * 文件上传测试
+     * 1.判断前端上传的文件是否为空
+     * 2.生成文件名的通用方法
+     *  ① 先获取文件后缀名
+     *  ② 生成 日期格式+随机数+文件后缀名 文件名
+     * 3.保存文件
+     * @param file
+     * @return
+     */
+    @PostMapping("/uploadFile")
+    @ResponseBody
+    public String uploadFile(@RequestParam("file")MultipartFile file){
+
+        //1.判断前端上传的文件是否为空
+        if(file.isEmpty()){
+            return "文件上传失败";
+        }
+        //① 先获取文件后缀名
+        String fileName = file.getOriginalFilename();
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        //② 生成 日期格式+随机数+文件后缀名 文件名
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        Random r = new Random();
+        StringBuilder tempName = new StringBuilder();
+        tempName.append(sdf.format(new Date())).append(r.nextInt(100)).append(suffixName);
+        String newFileName = tempName.toString();
+
+        try {
+            //3.保存文件
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(FILE_UPLOAD_PATH, newFileName);
+            Files.write(path, bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "文件上传失败";
+        }
+
+        return "文件上传成功, 地址为: /upload/" + newFileName;
+    }
+}
